462f794d76958fe24d95fa8ec6c22cebc806904b
Todd Motto <todd@toddmotto.com>
2016-03-18 00:52:50 +0000

    chore: Fixed directives

--- js/app/directives/Contact.js
+++ js/app/directives/Contact.js
@@ -3,7 +3,7 @@ function Contact() {
 		restrict: 'E',
 		template: [
 			'<div class="contact">',
-				'Name: {{ ctrl.contact.name.title }} {{ ctrl.contact.name.first }} {{ ctrl.contact.name.last }} - <a href="" ng-click="ctrl.remove()">Remove</a>',
+				'Name: {{ ctrl.contact.name.title }} {{ ctrl.contact.name.first }} {{ ctrl.contact.name.last }} - <a href="" ng-click="ctrl.remove(ctrl.id)">Remove</a>',
 			'</div>'
 		].join(''),
 		controller: function () {
